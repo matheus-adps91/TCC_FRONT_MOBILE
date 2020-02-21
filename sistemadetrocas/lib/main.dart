bf8e13c016dcafe4fafd63fe43a65536bebfdcdb
Matheus <matheus_adps@hotmail.com>
2020-02-21 00:10:43 -0300

    Design da tela de login

--- sistemadetrocas/lib/main.dart
+++ sistemadetrocas/lib/main.dart
@@ -7,6 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
       home: LoginPage(),
     );
   }
--- sistemadetrocas/lib/pages/login_page.dart
+++ sistemadetrocas/lib/pages/login_page.dart
@@ -3,8 +3,44 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sistema de Trocas"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
     return Container(
-      color: Colors.white,
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          Text('Login'),
+          TextFormField(),
+          Text('Senha'),
+          TextFormField(
+            obscureText: true,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            height: 46,
+            child: RaisedButton(
+              color: Colors.blue,
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
     );
   }
 }
